8e819b672335d0ef85768abad60eb31aaf5235cd
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-10-08 17:01:11 -0500

    Questions array

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "Please provide a projext title. (Required)",
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
