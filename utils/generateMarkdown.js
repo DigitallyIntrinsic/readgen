cc9abdf681467e1d92b8528ead34fa1d4096040b
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-10-09 17:32:43 -0500

    installation func

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,42 +2,53 @@
 // If there is no license, return an empty string
 const renderLicenseBadge = license => {
   if (license) {
-      return `![${license} License](https://img.shields.io/badge/license-${license.split(' ').join('%20')}-blue)
+    return `![${license} License](https://img.shields.io/badge/license-${license.split(' ').join('%20')}-blue)
 `;
   } else {
-      return '';
+    return '';
   }
 };
 const createDescription = (title, description, link) => {
   if (link) {
-      return `${description}
+    return `${description}
 
       View the deployed page at [${title}](${link}).`;
   } else {
-      return `${description}`;
+    return `${description}`;
   }
 };
 
 const createTableOfContents = contentsArr => {
   let contentsList = '';
   contentsArr.forEach((item) => {
-          if (item.content && item.header === 'Screenshots') {
-            contentsList += `   * [${item.header}](#${(item.header).toLowerCase()})
+    if (item.content && item.header === 'Screenshots') {
+      contentsList += `   * [${item.header}](#${(item.header).toLowerCase()})
       `;
-            } else if (item.content) {
-                contentsList += `* [${item.header}](#${(item.header).toLowerCase().split(' ').join('-')})
+    } else if (item.content) {
+      contentsList += `* [${item.header}](#${(item.header).toLowerCase().split(' ').join('-')})
       `;
-            }
-        });
-        return contentsList;
-      };
+    }
+  });
+  return contentsList;
+};
+
+const createInstallation = install => {
+  if (install) {
+    return `To use this application, please install: 
+      \`\`\`
+      ${install}
+      \`\`\``
+  } else {
+    return '';
+  }
+};
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
