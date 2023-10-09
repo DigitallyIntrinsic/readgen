038c8ae1139962f68cf9fd5cdb549bd9c0532985
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-10-09 17:27:34 -0500

    finalized TOC table

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,13 +11,26 @@ const renderLicenseBadge = license => {
 const createDescription = (title, description, link) => {
   if (link) {
       return `${description}
-      
+
       View the deployed page at [${title}](${link}).`;
   } else {
       return `${description}`;
   }
 };
 
+const createTableOfContents = contentsArr => {
+  let contentsList = '';
+  contentsArr.forEach((item) => {
+          if (item.content && item.header === 'Screenshots') {
+            contentsList += `   * [${item.header}](#${(item.header).toLowerCase()})
+      `;
+            } else if (item.content) {
+                contentsList += `* [${item.header}](#${(item.header).toLowerCase().split(' ').join('-')})
+      `;
+            }
+        });
+        return contentsList;
+      };
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
