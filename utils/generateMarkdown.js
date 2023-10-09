26929b8bbbdca7fd94d5a58450d5f93613aa07fe
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-10-09 17:39:10 -0500

    built with const

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -56,6 +56,20 @@ ${shot.screenshotDesc}
     return '';
   }
 };
+
+const createBuiltWith = builtWith =>{
+  let allTechnologies = '';
+
+  if (builtWith) {
+      builtWith.forEach(item => {
+          allTechnologies += `
+* ${item}`
+      });
+      return `${allTechnologies}`;
+  } else {
+      return '';
+  };
+};
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) { }
